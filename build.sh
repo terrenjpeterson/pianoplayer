0132b2257c792a781f8386eefc24ccf7a1774282
Terren Peterson <terrenjpeterson@yahoo.com>
2017-12-24 18:28:10 +0000

    added smoke test case

--- build.sh
+++ build.sh
@@ -14,3 +14,17 @@ rm pianobot.zip
 
 # update the lambda function with the binaries that have been staged
 aws lambda update-function-code --function-name pianoPlayerGreen --s3-bucket pianoplayerskill --s3-key binary/pianobot.zip
+
+# read in test data required to invoke the lambda function
+echo 'test case 1 - basic request'
+cd testing
+request=$(<request.json)
+cd ..
+
+# invoke the new lambda function
+aws lambda invoke --function-name pianoPlayerGreen --payload "$request" testOutput.json
+
+# read response file into local variable then print on the console
+response=$(<testOutput.json)
+echo $response
+echo 'test case 1 complete'
